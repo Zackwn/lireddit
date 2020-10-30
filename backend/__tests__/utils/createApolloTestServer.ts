1500e8fbc77857c1c18f7c69b41979fda4d24809
Zackwn <rafaelfavazzi@gmail.com>
2020-10-29 21:43:21 -0300

    helper to create a test app, server and database

---
+++ backend/__tests__/utils/createApolloTestServer.ts
@@ -0,0 +1,91 @@
+import { Connection, IDatabaseDriver, MikroORM } from '@mikro-orm/core'
+import { ApolloServer } from "apollo-server-express"
+import connectRedis from 'connect-redis'
+import express from 'express'
+import session from 'express-session'
+import Redis from 'ioredis'
+import 'reflect-metadata'
+import { buildSchema } from "type-graphql"
+import { COOKIE_NAME, __prod__ } from '../../src/constants'
+import { Post } from '../../src/entities/Post'
+import { User } from '../../src/entities/User'
+import { HelloResolver } from '../../src/resolvers/hello'
+import { PostResolver } from '../../src/resolvers/post/PostResolver'
+import { UserResolver } from '../../src/resolvers/user/UserResolver'
+import { createTestDatabaseConnection } from './createTestDatabaseConnection'
+
+export class appoloTest {
+  apolloServer: ApolloServer
+  expressApp: express.Express
+  private orm: MikroORM<IDatabaseDriver<Connection>>
+  private redis: Redis.Redis
+
+  public async stop() {
+    await this.apolloServer.stop()
+    await this.orm.close()
+    await this.redis.quit()
+    return
+  }
+
+  public async undo() {
+    await this.orm.em.nativeDelete(User, {})
+    await this.orm.em.nativeDelete(Post, {})
+  }
+
+  public async init() {
+    const orm = await createTestDatabaseConnection()
+    await orm.getMigrator().up()
+    this.orm = orm
+
+    const apolloTestServer = new ApolloServer({
+      schema: await buildSchema({
+        resolvers: [HelloResolver, PostResolver, UserResolver],
+        validate: false
+      }),
+      context: (req) => {
+        return {
+          em: orm.em,
+          redis: redisClient,
+          req: req.req,
+          res: req.res
+        }
+      }
+    })
+
+    this.apolloServer = apolloTestServer
+
+    const app = express()
+
+    const RedisStore = connectRedis(session)
+    const redisClient = new Redis()
+
+    this.redis = redisClient
+
+    app.use(session({
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 2, // 2 years
+        httpOnly: true,
+        sameSite: "lax",
+        secure: __prod__
+      },
+      name: COOKIE_NAME,
+      saveUninitialized: false,
+      secret: 'jnibhuvgkycfctyvgkujbhlkj',
+      resave: false,
+    }))
+
+    apolloTestServer.applyMiddleware({
+      app,
+      cors: false
+    })
+
+    this.expressApp = app
+    return
+  }
+
+  constructor() { }
+}
---
+++ backend/__tests__/utils/createTestDatabaseConnection.ts
@@ -0,0 +1,31 @@
+import path from "path";
+import { __prod__ } from "../../src/constants";
+import { Post } from "../../src/entities/Post";
+import { User } from "../../src/entities/User";
+import { MikroORM } from "@mikro-orm/core"
+
+const testConfig = {
+  migrations: {
+    path: path.resolve(__dirname, '..', '..', 'src', './migrations'),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+    allOrNothing: true
+  },
+  cache: {
+    enabled: false,
+  },
+  forceUtcTimezone: true,
+  entities: [
+    User, Post
+  ],
+  host: 'localhost',
+  dbName: 'lireddit_db_test',
+  type: 'postgresql',
+  user: 'postgres',
+  password: 'docker',
+  debug: false
+} as Parameters<typeof MikroORM.init>[0]
+
+export const createTestDatabaseConnection = async () => {
+  const orm = await MikroORM.init(testConfig)
+  return orm
+}
