65dd1c00119749fe27c67531403c7d26057042b7
Zackwn <rafaelfavazzi@gmail.com>
2020-11-03 16:42:33 -0300

    switching test server orm to typeorm

--- backend/__tests__/utils/testServer/createApolloTestServer.ts
+++ backend/__tests__/utils/testServer/createApolloTestServer.ts
@@ -1,4 +1,3 @@
-import { Connection, IDatabaseDriver, MikroORM } from '@mikro-orm/core'
 import { ApolloServer } from "apollo-server-express"
 import connectRedis from 'connect-redis'
 import express from 'express'
@@ -6,36 +5,45 @@ import session from 'express-session'
 import Redis from 'ioredis'
 import 'reflect-metadata'
 import { buildSchema } from "type-graphql"
+import { Connection, createConnection } from 'typeorm'
 import { TEST_COOKIE_NAME } from '../../../src/constants'
 import { Post } from '../../../src/entities/Post'
 import { User } from '../../../src/entities/User'
 import { HelloResolver } from '../../../src/resolvers/hello'
 import { PostResolver } from '../../../src/resolvers/post/PostResolver'
 import { UserResolver } from '../../../src/resolvers/user/UserResolver'
-import { createTestDatabaseConnection } from './createTestDatabaseConnection'
 
 export class appoloTest {
   apolloServer: ApolloServer
   expressApp: express.Express
-  private orm: MikroORM<IDatabaseDriver<Connection>>
+  conn: Connection
+  // private orm: MikroORM<IDatabaseDriver<Connection>>
   private redis: Redis.Redis
 
   public async stop() {
     await this.apolloServer.stop()
-    await this.orm.close()
+    await this.conn.close()
     await this.redis.quit()
     return
   }
 
   public async undo() {
-    await this.orm.em.nativeDelete(User, {})
-    await this.orm.em.nativeDelete(Post, {})
+    await User.delete({})
+    await Post.delete({})
   }
 
   public async init() {
-    const orm = await createTestDatabaseConnection()
-    await orm.getMigrator().up()
-    this.orm = orm
+    const conn = await createConnection({
+      type: 'postgres',
+      database: 'lireddit2_test',
+      password: 'docker',
+      username: 'postgres',
+      synchronize: true,
+      logging: false,
+      entities: [User, Post]
+    })
+
+    this.conn = conn
 
     const apolloTestServer = new ApolloServer({
       schema: await buildSchema({
@@ -44,7 +52,6 @@ export class appoloTest {
       }),
       context: (req) => {
         return {
-          em: orm.em,
           redis: redisClient,
           req: req.req,
           res: req.res
