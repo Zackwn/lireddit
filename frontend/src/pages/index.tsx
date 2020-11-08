fb6d48ee489134b576b0f973256bdd50e641b09e
Zackwn <rafaelfavazzi@gmail.com>
2020-11-08 16:13:29 -0300

    styling posts

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,25 +4,49 @@ import { createUrqlClient } from '../utils/createUrqlClient';
 import { usePostsQuery } from '../generated/graphql';
 import Layout from '../components/Layout';
 import NextLink from 'next/link'
-import { Link } from '@chakra-ui/core';
+import { Link, Stack, Flex, Heading, Button } from '@chakra-ui/core';
+import Post from '../components/Post'
 
 const Index: React.FC = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
-      limit: 2
+      limit: 10
     }
   })
 
+  if (!data && !fetching) {
+    return <div>You got query failed for some reason</div>
+  }
+
   return (
     <Layout>
-      {/* <div>Langing page</div> */}
-      <NextLink href='/create-post'>
-        <Link>create post</Link>
-      </NextLink>
-      {!data ? null : data.posts.map((p) => (
-        <div key={p.id}>{p.title}</div>
-      ))}
-    </Layout>
+      <Flex
+        justifyContent='space-between'
+        alignItems='center'
+        mb={5}
+      >
+        <Heading>LiReddit</Heading>
+        <NextLink href='/create-post'>
+          <Link>create post</Link>
+        </NextLink>
+      </Flex>
+      <Stack spacing={8}>
+        {!data ? null : data.posts.map((post) => (
+          <Post
+            key={post.id}
+            title={post.title}
+            text={post.textSnippet}
+          />
+        ))}
+      </Stack>
+      {data ? <Flex>
+        <Button
+          m='auto'
+          my={8}
+          isLoading={fetching}
+        >load more</Button>
+      </Flex> : null}
+    </Layout >
   )
 }
 
