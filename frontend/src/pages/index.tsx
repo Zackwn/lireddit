435b9c69fe86a130d302e5e7fb657e6dae42e239
Zackwn <rafaelfavazzi@gmail.com>
2020-11-08 14:20:49 -0300

    pagination

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,7 +7,11 @@ import NextLink from 'next/link'
 import { Link } from '@chakra-ui/core';
 
 const Index: React.FC = () => {
-  const [{ data }] = usePostsQuery()
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 2
+    }
+  })
 
   return (
     <Layout>
